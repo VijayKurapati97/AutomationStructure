69bbcc318965b4b092b944680dfc6b336b7acb67
Vijay Kumar <vijay.kurapati@outlook.com>
2022-10-31 15:00:53 +0530

    Fixed getData method.

--- src/main/java/com/finvisage/utils/DataProviderUtils.java
+++ src/main/java/com/finvisage/utils/DataProviderUtils.java
@@ -1,40 +1,33 @@
 package com.finvisage.utils;
 
+import org.testng.annotations.DataProvider;
+
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-import org.testng.annotations.DataProvider;
-
 public final class DataProviderUtils {
-	
-	@DataProvider
-	public static Object[][] getData(Method m) {
-		
- 	List<Map<String,String>> list;
- 	Object[][] objects = null;
-		
-		String testName = m.getName();
-		list = ExcelUtils.getTestDetails("LoginData");
-		List<Map<String,String>> smallList=new ArrayList<>();
-
-		for (Map<String, String> stringStringMap : list) {
-
-			if (stringStringMap.get("TestName").equalsIgnoreCase(testName ) && stringStringMap.get("Execute").equalsIgnoreCase("yes")) {
-				smallList.add(stringStringMap);
-			}
-		}
-		list.removeAll(smallList);
-
-		// TODO :: Need to Fix This!!!
-
-		if(!smallList.isEmpty()) {
-			objects = new Object[smallList.size()][1];
-		}
 
 
-		return objects;
-	}
+    @DataProvider
+    public static Object[] getData(Method m) {
+
+        List<Map<String, String>> list;
+
+        String testname = m.getName();
+        list = ExcelUtils.getTestDetails("LoginData");
+        List<Map<String, String>> smallList = new ArrayList<>();
+
+        for (Map<String, String> stringStringMap : list) {
+
+            if (stringStringMap.get("TestName").equalsIgnoreCase(testname) && stringStringMap.get("Execute").equalsIgnoreCase("yes")) {
+
+                smallList.add(stringStringMap);
+            }
+        }
+        list.removeAll(smallList);
+        return smallList.toArray();
+    }
 
 }
--- src/main/java/com/finvisage/utils/ExcelUtils.java
+++ src/main/java/com/finvisage/utils/ExcelUtils.java
@@ -1,64 +1,56 @@
 package com.finvisage.utils;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
-
+import com.finvisage.constants.FrameworkConstants;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
-import com.finvisage.constants.FrameworkConstants;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.*;
 
 public final class ExcelUtils {
 
-	private ExcelUtils() {}
+    public static List<Map<String, String>> getTestDetails(String sheetname) {
+        FileInputStream fs = null;
+        List<Map<String, String>> list = new ArrayList<>();
+        Map<String, String> map;
+        try {
+            fs = new FileInputStream(FrameworkConstants.getExcelpath());
+            @SuppressWarnings("resource")
+            XSSFWorkbook book = new XSSFWorkbook(fs);
+            XSSFSheet sheet = book.getSheet(sheetname);
 
-	public static List<Map<String, String>> getTestDetails(String sheetname) {
-		FileInputStream fs = null;
-		List<Map<String,String>>list=new ArrayList<>();
-		Map<String,String> map;
-		try {
-		 fs=new FileInputStream(FrameworkConstants.getExcelpath());
+            int lastRowNum = sheet.getLastRowNum();
+            int lastColumnNum = sheet.getRow(0).getLastCellNum();
+            for (int i = 1; i <= lastRowNum; i++) {
 
-		XSSFWorkbook book=new XSSFWorkbook(fs);
-			XSSFSheet sheet=book.getSheet(sheetname);
-			
-			int lastRowNum=sheet.getLastRowNum();
-			int lastColumnNum=sheet.getRow(0).getLastCellNum();
-			for(int i=1;i<=lastRowNum;i++) {
-				
-			map= new HashMap<>();
-			
-			for(int j=0;j<lastColumnNum;j++) {
-				
-		String key=sheet.getRow(0).getCell(j).getStringCellValue();
-		String value=sheet.getRow(i).getCell(j).getStringCellValue();
-		map.put(key,value);
-		
-			}
-			list.add(map);
-			
-			}
-			
-		} catch (IOException e) {
-			e.printStackTrace();
-		} finally {
-			
-			if(Objects.nonNull(fs)) {
-			try {
-				fs.close();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-			}
-		}
-		return list;
-	
-	}
+                map = new HashMap<>();
+
+                for (int j = 0; j < lastColumnNum; j++) {
+
+                    String key = sheet.getRow(0).getCell(j).getStringCellValue();
+                    String value = sheet.getRow(i).getCell(j).getStringCellValue();
+                    map.put(key, value);
+
+                }
+                list.add(map);
+
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+
+            if (Objects.nonNull(fs)) {
+                try {
+                    fs.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return list;
+
+    }
 
 }
