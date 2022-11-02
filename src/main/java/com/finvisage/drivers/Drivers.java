83d26fbec2725290253c7760fab79962a0c072e6
Vijay Kumar <vijay.kurapati@outlook.com>
2022-11-02 15:14:19 +0530

    firefox and Edge browsers are added.

--- src/main/java/com/finvisage/drivers/Drivers.java
+++ src/main/java/com/finvisage/drivers/Drivers.java
@@ -9,6 +9,8 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import com.finvisage.constants.FrameworkConstants;
 import com.finvisage.enums.ConfigProperties;
 import com.finvisage.utils.PropertyFileReader;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public final class Drivers {
 
@@ -18,9 +20,23 @@ public final class Drivers {
 
     public static void initDriver() throws Exception {
         if (Objects.isNull(DriverManager.getDriver())) {
-            WebDriverManager.chromedriver().setup();
-            WebDriver driver = new ChromeDriver();
-            DriverManager.setDriver(driver);
+            if(PropertyFileReader.get(ConfigProperties.BROWSER).equalsIgnoreCase("chrome")||
+                    PropertyFileReader.get(ConfigProperties.BROWSER).equalsIgnoreCase("")) {
+                WebDriverManager.chromedriver().setup();
+                WebDriver driver = new ChromeDriver();
+                DriverManager.setDriver(driver);
+            }
+            else if(PropertyFileReader.get(ConfigProperties.BROWSER).equalsIgnoreCase("firefox")){
+                WebDriverManager.firefoxdriver().setup();
+                WebDriver driver = new FirefoxDriver();
+                DriverManager.setDriver(driver);
+            }
+            else if(PropertyFileReader.get(ConfigProperties.BROWSER).equalsIgnoreCase("Edge")){
+                WebDriverManager.edgedriver().setup();
+                WebDriver driver = new EdgeDriver();
+                DriverManager.setDriver(driver);
+            }
+
             DriverManager.getDriver().get(PropertyFileReader.get(ConfigProperties.URL));
         }
 
