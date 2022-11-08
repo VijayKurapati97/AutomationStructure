65932e5f3f8e3bf164092c6d39dae489f470a891
Vijay Kumar <vijay.kurapati@outlook.com>
2022-11-08 14:09:44 +0530

    Added Headless mode.

--- src/main/java/com/finvisage/drivers/Drivers.java
+++ src/main/java/com/finvisage/drivers/Drivers.java
@@ -12,8 +12,11 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import com.finvisage.constants.FrameworkConstants;
 import com.finvisage.enums.ConfigProperties;
 import com.finvisage.utils.PropertyFileReader;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 
 public final class Drivers {
     private  static Logger logger = LogManager.getLogger(LogInPage.class);
@@ -21,22 +24,41 @@ public final class Drivers {
 
     public static void initDriver() throws Exception {
         if (Objects.isNull(DriverManager.getDriver())) {
+            WebDriver driver;
             if(PropertyFileReader.get(ConfigProperties.BROWSER).equalsIgnoreCase("chrome")||
                     PropertyFileReader.get(ConfigProperties.BROWSER).equalsIgnoreCase("")) {
                 WebDriverManager.chromedriver().setup();
-                WebDriver driver = new ChromeDriver();
+               if(PropertyFileReader.get(ConfigProperties.HEADLESS).equalsIgnoreCase("yes")){
+                   ChromeOptions options = new ChromeOptions();
+                   options.addArguments("--headless");
+                   driver = new ChromeDriver(options);
+               }else {
+                   driver = new ChromeDriver();
+               }
                 DriverManager.setDriver(driver);
                 logger.info("--chrome driver launched--Successfully");
             }
             else if(PropertyFileReader.get(ConfigProperties.BROWSER).equalsIgnoreCase("firefox")){
                 WebDriverManager.firefoxdriver().setup();
-                WebDriver driver = new FirefoxDriver();
+                if(PropertyFileReader.get(ConfigProperties.HEADLESS).equalsIgnoreCase("yes")){
+                    FirefoxOptions options =new FirefoxOptions();
+                    options.addArguments("--headless");
+                    driver = new FirefoxDriver(options);
+                }else {
+                    driver = new FirefoxDriver();
+                }
                 DriverManager.setDriver(driver);
                 logger.info("--firefox driver launched--Successfully");
             }
             else if(PropertyFileReader.get(ConfigProperties.BROWSER).equalsIgnoreCase("Edge")){
                 WebDriverManager.edgedriver().setup();
-                WebDriver driver = new EdgeDriver();
+                if(PropertyFileReader.get(ConfigProperties.HEADLESS).equalsIgnoreCase("yes")){
+                    EdgeOptions options =new EdgeOptions();
+                    options.addArguments("--headless");
+                    driver = new EdgeDriver(options);
+                }else {
+                    driver = new EdgeDriver();
+                }
                 DriverManager.setDriver(driver);
                 logger.info("--edge driver launched--Successfully");
             }
