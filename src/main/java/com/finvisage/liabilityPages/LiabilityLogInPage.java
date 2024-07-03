8e95cb37dce40ee8921ed68bcbabb92113cee5f9
Vijay <vijaykurapati@apexft.com>
2024-07-03 15:04:15 +0530

    minor changes

--- src/main/java/com/finvisage/liabilityPages/LiabilityLogInPage.java
+++ src/main/java/com/finvisage/liabilityPages/LiabilityLogInPage.java
@@ -3,15 +3,14 @@ package com.finvisage.liabilityPages;
 import com.finvisage.enums.WaitStrategy;
 import org.openqa.selenium.By;
 
-public final class LiabiltyLogInPage extends BasePageLiability {
+public final class LiabilityLogInPage extends BasePageLiability {
     private final By UserName = By.id("email-input");
     private final By Password = By.xpath("//input[@id='password-input']");
     private final By btnLogIn = By.xpath("//*[text()='Log In']");
 
-    public LiabiltyLogInPage enterUserNameAndPassword(String[] Username) {
+    public void enterUserNameAndPassword(String[] Username) {
         sendText(UserName, Username[0], WaitStrategy.PRESENCE, "Email box");
-        sendText(Password, Username[1], WaitStrategy.PRESENCE, "passworddd box");
-        return this;
+        sendText(Password, Username[1], WaitStrategy.PRESENCE, "password box");
     }
 
     public void clickLogIn() {
