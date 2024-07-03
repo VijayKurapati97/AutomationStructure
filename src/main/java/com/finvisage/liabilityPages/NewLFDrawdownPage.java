ad67d4f1a010305874139051193ce0ce6e0c681e
Vijay <vijaykurapati@apexft.com>
2024-07-03 15:06:19 +0530

    Added methods and xpaths to handle Loan Facility test cases

--- src/main/java/com/finvisage/liabilityPages/NewLFDrawdownPage.java
+++ src/main/java/com/finvisage/liabilityPages/NewLFDrawdownPage.java
@@ -4,9 +4,7 @@ import com.finvisage.drivers.DriverManager;
 import com.finvisage.enums.WaitStrategy;
 import com.finvisage.utils.XpathUtils;
 import com.google.common.util.concurrent.Uninterruptibles;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 
 import java.util.concurrent.TimeUnit;
 
@@ -15,17 +13,17 @@ public class NewLFDrawdownPage extends BasePageLiability {
     private final By drawdownLedgerID = By.xpath("//label[text()='Ledger ID']/parent::div/input");
     private final By drawdownValueDate = By.id("loan_facility_drawdown_start_date");
     private final By getDrawdownEndDate = By.id("loan_facility_drawdown_end_date");
-    private final By prepayments = By.xpath("//select[@id='prepayment_allowed_select']/following-sibling:: div/div[1]");
+    // private final By prepayments = By.xpath("//select[@id='prepayment_allowed_select']/following-sibling:: div/div[1]");
     private final By penalty = By.id("prepayment_penalty_number");
-    //   private final By lockInPeriod = By.id("lock_in_period_number");
+    //private final By lockInPeriod = By.id("lock_in_period_number");
     private final By put_call = By.xpath("//label[text()='Put/Call *']//parent::div/div/div");
-    private final By loanAccount = By.xpath("//label[text()=' Loan Account *']/parent::div/input");
+    private final By loanAccount = By.xpath("//label[text()='Loan Account *']//parent::div/input");
     private final By operatingAccount = By.xpath("//select[@id='liability_bank_account_select']" +
             "/following-sibling::div/div[1]");
     private final By paymentAccount = By.xpath("//select[@id='loan_facility_dropdown_payment_account_" +
             "option']/following-sibling::div/div[1]");
-    private final By counterpartyBankAccount = By.xpath("//select[@id='loan_facility_dropdown_counterparty" +
-            "_bank_account_id']/following-sibling::div/div[1]");
+    /*private final By counterpartyBankAccount = By.xpath("//select[@id='loan_facility_dropdown_counterparty" +
+            "_bank_account_id']/following-sibling::div/div[1]");*/
     private final By newDisbursement = By.xpath("//a[text()='New Disbursement']");
     private final By disbursementAmount = By.xpath("//label[text()='Disbursement Amount *']//parent::div/input");
 
@@ -38,6 +36,8 @@ public class NewLFDrawdownPage extends BasePageLiability {
     private final By TDS = By.xpath("//label[text()='TDS *']//parent:: div/input");
     private final By additionalInfo = By.xpath("//label[text()=' Additional Info *']//parent:: div/textarea");
     private final By btnCreate = By.xpath("//input[@name='commit']");
+    private final By btn_SaveAsDraft = By.xpath("//a[text()='Cancel']//following-sibling::a");
+    private final By btn_cancel = By.xpath("//a[text()='Cancel']");
 
     public NewLFDrawdownPage enterDrawdownExternalID(int count) {
         String randomID = generateRandomID(count, "LoanFacility-Drawdown");
@@ -45,7 +45,7 @@ public class NewLFDrawdownPage extends BasePageLiability {
         return this;
     }
 
-    public NewLFDrawdownPage enterDrwadownLedgerID(int count) {
+    public NewLFDrawdownPage enterDrawdownLedgerID(int count) {
         String randomID = generateRandomID(count, "LoanFacility-Drawdown");
         sendText(drawdownLedgerID, String.valueOf(randomID), WaitStrategy.PRESENCE, "Drawdown-ledger ID");
         return this;
@@ -63,9 +63,9 @@ public class NewLFDrawdownPage extends BasePageLiability {
         return this;
     }
 
-    public NewLFDrawdownPage selectPrepayemntsPenalty(String penaltyValue) {
-        jsClick(penalty, "penlty");
-       WebElement ele= DriverManager.getDriver().findElement(penalty);
+    public NewLFDrawdownPage selectPrepaymentsPenalty(String penaltyValue) {
+        jsClick(penalty, "penalty");
+        WebElement ele = DriverManager.getDriver().findElement(penalty);
         ele.sendKeys(Keys.ARROW_LEFT);
         ele.sendKeys(Keys.ARROW_LEFT);
         ele.sendKeys(Keys.ARROW_LEFT);
@@ -79,7 +79,7 @@ public class NewLFDrawdownPage extends BasePageLiability {
         return this;
     }
 
-    public NewLFDrawdownPage selectPayementAcnt(String text) {
+    public NewLFDrawdownPage selectPaymentAcnt(String text) {
         jsClick(paymentAccount, WaitStrategy.CLICKABLE, "Payment Account");
         Uninterruptibles.sleepUninterruptibly(3, TimeUnit.SECONDS);
         String ar = "//div[text()='%replace%']";
@@ -104,7 +104,7 @@ public class NewLFDrawdownPage extends BasePageLiability {
 
     public NewLFDrawdownPage enterDisAmount(String value) {
         clickk(disbursementAmount, WaitStrategy.CLICKABLE, "disbursement amount");
-        sendText(disbursementAmount, value, WaitStrategy.PRESENCE, "--");
+        sendText(disbursementAmount, value, WaitStrategy.PRESENCE, "disbursement amount");
         return this;
     }
 
@@ -130,10 +130,9 @@ public class NewLFDrawdownPage extends BasePageLiability {
         String newxpath = XpathUtils.getXpath(ar, text);
         clickk(By.xpath(newxpath), WaitStrategy.CLICKABLE, text);
         if (getText(interestType, WaitStrategy.VISIBLE, "Interest Type").equalsIgnoreCase("floating")) {
-            jsClick(interestBenchmark, WaitStrategy.CLICKABLE, "Benchamrk");
-            Uninterruptibles.sleepUninterruptibly(2, TimeUnit.SECONDS);
-            jsClick(By.xpath("//div[text()='AUTOMATION_MARK_123 - 5years (05/11/2018) (12.0%)']"),
-                    "Benchmark value");
+            Uninterruptibles.sleepUninterruptibly(4, TimeUnit.SECONDS);
+            jsClick(interestBenchmark, WaitStrategy.CLICKABLE, "Benchmark");
+            actionSendkeys("AUTOMATION_MARK_123");
         }
         return this;
     }
@@ -178,5 +177,27 @@ public class NewLFDrawdownPage extends BasePageLiability {
         return new LoanFacilityDrawdownPage();
     }
 
+    //TO DO
+    //clickCancel() method is added because after clicking save as draft button it's not navigating to Loan Facility page
+    public NewLFDrawdownPage clickSaveAsDraft() {
+        for (int i = 0; i < 10; i++) {
+            clickk(btn_SaveAsDraft, WaitStrategy.CLICKABLE, "Save as draft");
+            try {
+                scrollIntoView(btn_SaveAsDraft);
+                clickk(btn_SaveAsDraft, WaitStrategy.CLICKABLE, "Save as draft");
+                Alert al = DriverManager.getDriver().switchTo().alert();
+                al.accept();
+                break;
+            } catch (NoAlertPresentException e) {
+                Uninterruptibles.sleepUninterruptibly(2, TimeUnit.SECONDS);
+            }
+        }
+        return this;
+    }
+
+    public LoanFacilityPage clickCancel() {
+        clickk(btn_cancel, WaitStrategy.CLICKABLE, "cancel button");
+        return new LoanFacilityPage();
+    }
 
 }
